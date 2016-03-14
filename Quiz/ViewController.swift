8a00c9b45675596cc4b0865b2b4753d739e82ccc
synesthesio <msinger681@gmail.com>
2016-03-14 19:06:59 -0400

    added connections

--- Quiz/ViewController.swift
+++ Quiz/ViewController.swift
@@ -9,6 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController {
+@IBOutlet var questionLabel:UILabel!
+@IBOutlet var answerLabel:UILabel!
+
+	@IBAction func showNextQuestion(sender:AnyObject) {
+		
+	}
+	@IBAction func showAnswer(sender:AnyObject) {
+		
+	}
+
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
